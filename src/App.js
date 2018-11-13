06fea5728ebf3f04a2fb29097e7667a0537bd763
Rajat Venkatesh <vrajat@dblint.io>
2018-11-13 14:29:17 +0530

    new:usr:Create a text box and submit form to accept SQL
    Create a form to accept a SQL String and a submit button. 
    The submit button does not take any action right now.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
+import SqlText from './components/SqlText'
 
 const App = () => {
-  return (<div className="App">DbLint coming soon!</div>);
+  return (<div className="App"><SqlText/></div>);
 };
 
 export default App;
