14d44ffe874724dc4b445dd1b0a22df92ff951d3
Rajat Venkatesh <vrajat@dblint.io>
2018-11-17 10:25:55 +0530

    fix:usr:Fix ahref in Header to point to homepage.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import { Navbar } from 'react-bootstrap';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: window.location.protocol + "//" + window.location.host
+    };
+
+  }
+
   render() {
     return (
       <div>
       <Navbar inverse>
         <Navbar.Header>
             <Navbar.Brand>
-              <a href="#">DbLint</a>
+              <a href={this.state.url}>DbLint</a>
             </Navbar.Brand>
             <Navbar.Text color="#fff"> Tools for the Database Engineer</Navbar.Text>
         </Navbar.Header>
