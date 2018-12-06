f57c90bb6dc147caccc6f70f31bf3757465a7147
Rajat Venkatesh <vrajat@dblint.io>
2018-12-06 13:04:58 +0530

    Fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,70 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import FontAwesome from "react-fontawesome";
+import { Button, Col, Container, Row } from "reactstrap";
+import { version } from "../constants";
+import axios from 'axios';
 
 const propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node
 };
 
 const defaultProps = {};
 
 class Footer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.versions = {
+      martVersion: "Unknown",
+      feVersion: version
+    }
+  }
+
+  componentDidMount() {
+    let self = this;
+    axios({
+      method: 'get',
+      url: "/api/dblint/version",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      }
+    }).then(function (response) {
+      self.setState({martVersion: response.data.buildVersion});
+    }).catch(function (error) {
+      console.log(error);
+    });
+  }
+
   render() {
-
-    // eslint-disable-next-line
-    const { children, ...attributes } = this.props;
-
     return (
-      <React.Fragment>
-        <span> &copy; 2018 <a href="https://dblint.io">Dblint</a></span>
-        <span className="ml-auto">
-          <FontAwesome name="github" size="2x"/>
-          <a href="https://github.com/dblintio"> Github Project</a>
-        </span>
-      </React.Fragment>
+      <Container>
+        <Row>
+          <Col sm={4}> &copy; 2018 <a href="https://dblint.io">Dblint</a></Col>
+          <Col className="ml-auto" sm={4}>
+            <Button color="danger" size="sm"
+                    href="https://github.com/dblintio/frontend/issues">
+              <FontAwesome name="bug"/>
+              Report Issues
+            </Button>
+          </Col>
+          <Col xs={2}>
+            <Button color="info" size="sm"
+                    href="https://github.com/dblintio/frontend/releases">
+              <FontAwesome name="github"/>
+              Frontend Version: {version}
+            </Button>
+          </Col>
+          <Col xs={2}>
+            <Button color="info" size="sm"
+                    href="https://github.com/dblintio/mart/releases">
+              <FontAwesome name="github"/>
+              Mart Version: Unknown
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
